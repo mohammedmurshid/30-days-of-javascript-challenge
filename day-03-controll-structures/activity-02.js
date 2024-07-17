17b7beb72881c95d04c398303d1b884588694d8c
Mohammed Murshid <murshid434@gmail.com>
2024-07-17 23:28:35 +0530

    learnings: day 03 - if else statements

---
+++ day-03-controll-structures/activity-02.js
@@ -0,0 +1,18 @@
+// Task 03
+const a = 192;
+const b = 348;
+const c = 41235;
+
+if (a >= b) {
+  if (a >= c) {
+    console.log(`${a} is bigger`);
+  } else {
+    console.log(`${c} is bigger`);
+  }
+} else {
+  if (b >= c) {
+    console.log(`${b} is bigger`);
+  } else {
+    console.log(`${c} is bigger`);
+  }
+}
